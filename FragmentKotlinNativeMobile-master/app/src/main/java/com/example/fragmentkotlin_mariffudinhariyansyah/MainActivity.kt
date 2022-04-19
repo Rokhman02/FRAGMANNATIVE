00b87c3b8609cd8c25d97eaa4bca7f22fdb7efef
Rokhman02 <102138686+Rokhman02@users.noreply.github.com>
2022-04-19 20:55:09 +0700

    Add files via upload

---
+++ FragmentKotlinNativeMobile-master/app/src/main/java/com/example/fragmentkotlin_mariffudinhariyansyah/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragmentkotlin_mariffudinhariyansyah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flfragment, firstFragment)
+            commit()
+        }
+
+        val btnFirstFragment = findViewById<Button>(R.id.btnFirstFragment)
+
+        btnFirstFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flfragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        val btnSecondFragment = findViewById<Button>(R.id.bntSecondFragment)
+
+        btnSecondFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flfragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+
+
+        }
+    }
+}
